a5b194f6594f9258b36ec54f1ab342cc9ea82131
Viktoria Boiko <170492v@gmail.com>
2023-02-13 01:08:08 +0300

    add seminar 02

---
+++ Seminar02/task_01/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10, 99] и
+//  показывает наибольшую цифру числа.
+//  Например:
+//  78 -> 8
+//  12 -> 2
+//  85 -> 8
+
+Random rand = new Random();
+int randNumber = rand.Next(10, 100);
+
+System.Console.WriteLine(randNumber);
+
+int leftNumber = randNumber / 10;
+int rightNumber = randNumber % 10;
+
+if (leftNumber > rightNumber)
+{
+    System.Console.WriteLine($"{leftNumber} > {rightNumber}");
+}
+else if (rightNumber > leftNumber)
+{
+    System.Console.WriteLine($"{rightNumber} > {leftNumber}");
+}
+else{
+    System.Console.WriteLine("Числа равны");
+}
---
+++ Seminar02/task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая выводит случайное трехзначное число
+//  и удаляет вторую цифру этого числа.
+//  456 -> 46
+//  782 -> 72
+//  918 -> 98
+
+Random rand = new Random();
+int RandomN = rand.Next(100, 1000);
+
+System.Console.WriteLine(RandomN);
+
+// int leftN = RandomN / 100;
+// int rightN = RandomN % 10;
+// System.Console.WriteLine(leftN + "" + rightN);
+
+int leftN = RandomN / 100 * 10;
+int rightN = RandomN % 10;
+int sum = leftN + rightN;
+System.Console.WriteLine(sum);
