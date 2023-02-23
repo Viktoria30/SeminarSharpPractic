da248a7d1dbc8cd1ea9e7ead5d0069c404c9f477
Viktoria Boiko <170492v@gmail.com>
2023-02-23 14:27:36 +0300

    save seminar04

---
+++ Seminsr04/task_03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+static int ReadInt(string prompt)
+{
+    Console.WriteLine(prompt);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+static int Factorial(int number)
+{
+    int result = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        result = result * i; // result *= i;
+    }
+    return result;
+}
+
+Console.WriteLine(Factorial(ReadInt("Enter a number: ")));
+
